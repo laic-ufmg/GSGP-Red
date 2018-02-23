b15ad515e8d7e4ac4491feef88245866d67a3376
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2018-02-23 16:53:14 -0300

    Parser and evaluation working

--- scripts/gsgp3-tree-parser.py
+++ scripts/gsgp3-tree-parser.py
@@ -5,42 +5,50 @@
 
 # Packages to import
 import pandas as pd
-import numpy as np
+from math import sqrt
 
 # In[ ]:
 
-func_set = ['*', '+', '-', 'AQ']
+operators = {'*': lambda x, y : x * y, 
+            '+': lambda x, y : x + y, 
+            '-': lambda x, y : x - y,
+            'AQ': lambda x, y : x / sqrt(1 + y**2) 
+            }
 delimiters = ["(", ")"]
 
 # A binary tree node 
 class Node:
     
     # Constructor to create a new node
-    def __init__(self):
-        self.data = None
+    def __init__(self, data = None):
+        self.data = data
         self.left = None
         self.right = None
-        self.parent = None
     
     def __str__(self):
-        output_str = ""
+#        output_str = ""
+#        if self.left:
+#            output_str += str(self.left.data)
+#        else:
+#            output_str += "None"
+#        
+#        output_str += "  <  " + str(self.data) + "  >  "
+#        
+#        if self.right:
+#            output_str += str(self.right.data)
+#        else:
+#            output_str += "None"
         
-        if self.left:
-            output_str += self.left.data
-        else:
-            output_str += "None"
-        
-        output_str += ", " + self.data + ", "
-        
-        if self.right:
-            output_str += self.right.data
-        else:
-            output_str += "None"
-        
-        return output_str
+        return str(self.data)
     
     def __repr__(self):
         return self.__str__()
+    
+    def is_leaf(self):
+        return not (self.left or self.right)
+    
+    def is_operator(self):
+        return self.data in operators
     
     def prefix_str(self):
         aux_stack = [self]
@@ -68,6 +76,77 @@ class Node:
                 output_str += str(node.data) + " "
                 node = node.right                
         return output_str
+    
+    def get_value(self, input_data):
+        if self.data in input_data:
+            new_node = Node(data = input_data[self.data])
+        else:
+            new_node = Node(data = float(self.data))
+        return new_node
+    
+    def evaluate(self, input_data):
+        # In case the tree is compposed by a constant
+        if not self.is_operator():
+            return self.get_value(input_data).data
+        
+        parent = self
+        node = self.left
+        stack = [self]
+        
+        while node:
+            if node.is_operator():
+                stack.append(node)
+            else:
+                stack.append(node.get_value(input_data))
+            
+            if node.is_leaf():
+                # node is a left child
+                if node == parent.left:
+                    node = parent.right
+                # node is a right child
+                else:
+                    while True:
+                        term_2 = stack.pop().data
+                        term_1 = stack.pop().data
+                        op = operators[stack.pop().data]
+                        result = op(term_1, term_2)
+                        if not stack:
+                            return result
+                        else:
+                            stack.append(Node(data = result))
+                        
+                        if not stack or stack[-2].is_operator():
+                            node = stack[-2].right
+                            break                    
+            else:
+                parent = node
+                node = node.left
+        
+                
+        
+    
+def lin_comb_from_gsgp3_tree(gsgp3_parse_tree):
+     el_list = []
+     node = gsgp3_parse_tree
+     if not node or node.data != '+':
+         raise SyntaxError("The root node must be an '+' operator.")
+     while node and node.right.data == "+":
+         lin_comb_term = node.left
+         # Check if the coefficient + subtree is OK
+         if lin_comb_term.data != "*":
+             raise SyntaxError("The root of a linear combination term must be '*'.")
+         if not lin_comb_term.left:
+             raise SyntaxError("The coefficient of a linear combination term must not be 'None'.")
+         if not lin_comb_term.right:
+             raise SyntaxError("The function termo of a linear combination term must not be 'None'.")
+         # Add the linear combination term to the list
+         # Notice that the coefficient is implicitly cast to float
+         el_list.append([float(lin_comb_term.left.data), lin_comb_term.right])
+         node = node.right
+         
+     return el_list
+
+    
 
 def build_parse_tree(exp_string):
     # Split the string by white space
@@ -91,67 +170,37 @@ def build_parse_tree(exp_string):
         
         #print(fmt.format(token, len(aux_stack), len(token_stack), debug_stack[-5:]))        
         
-        if token in func_set:
+        if token in operators:
             node.right = Node()
-            node.right.parent = node
             aux_stack.append(node.right)
             node.left = Node()
-            node.left.parent = node
             aux_stack.append(node.left)
     return root
-
 
 # In[ ]:
 
 exp = open('/home/luiz/Dados/Trabalho/Pesquisa/Projetos/GSGP3/scripts/airfoil.out', 'r').readline().rstrip()
-#tree = build_parse_tree("+ ( * ( c1 t1 ) * ( c2 t2 ) )")
-tree = build_parse_tree(exp)
+sol = build_parse_tree(exp)
+lin_comb = lin_comb_from_gsgp3_tree(sol)
+df = pd.DataFrame(lin_comb, columns = ["Coeff", "Function"])
+
+# In[]:
+
+df = df.assign(Coeff_Abs = df.Coeff.abs())
+df = df.assign(Coeff_Rel = df.Coeff_Abs / df.Coeff.sum())
+
+# In[]:
 
 
-# In[ ]:
 
+# In[]:
 
+data = pd.read_csv('/home/luiz/Dropbox/Pesquisa/luiz_otavio_operador_semantico/'
+                   'dados/original/airfoil-train-0.dat', header = None)
+# Rename columns according to variable names
+names = ["x" + str(i) for i in range(data.shape[1]-1)] + ["y"]
+data = data.rename(columns = lambda i : names[i])
 
-print(tree.prefix_str())
-
-
-# In[ ]:
-
-
-def check_par(exp_string):
-    # Split the string by white space
-    token_stack = exp_string.split(" ")
-    token_stack.reverse()
-    space_stack = [0]
-    while token_stack:
-        token = token_stack.pop()
-        if token not in [")", "("]:
-            print(token, end = " ")
-        if token == "(":
-            space_stack.append(space_stack[-1] + 3)
-            print("(\n" + " " * space_stack[-1], end = "")
-        if token == ")":
-            print("\n" + " " * (space_stack.pop() - 3) + ")")
-            
-    
-
-
-# In[ ]:
-
-
-def read_tokens(token_stack, tree):
-    term = None
-    
-    #set_trace()
-    
-    while (term == None or term in delimiters) and token_stack:
-        term = token_stack.pop()
-    
-    tree.data = term
-    if term in func_set and term != None:
-        tree.left = Node()
-        read_tokens(token_stack, tree.left)
-        tree.right = Node()
-        read_tokens(token_stack, tree.right)
-
-        
+n_col = data.shape[1]
+for i in range(df.shape[0]):
+    data.insert(n_col + i, "f" + str(i), data.apply(df.iloc[i,1].evaluate, axis = 1))
